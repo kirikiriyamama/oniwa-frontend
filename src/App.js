75e5c857f31183a33ad42246afdced6f868d8d55
kirikiriyamama <kirikiriyamama@gmail.com>
2020-06-14 16:56:20 +0900

    Show activities with dummy data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import React from 'react';
 
-function App() {
-  return <h1>Hello, world!</h1>;
+class App extends React.Component {
+  render() {
+    const data = [
+      {
+        id: '1',
+        date: '2020-06-01T00:00:00+09:00',
+        vegetable: 'ピーマン',
+        job: '肥料',
+        note: 'おおきくなーれ'
+      },
+      {
+        id: '2',
+        date: '2020-06-02T00:00:00+09:00',
+        vegetable: 'ピーマン',
+        job: '収穫',
+        note: 'おおきくなった'
+      }
+    ];
+
+    const activities = data.map((activity) => {
+      const date = new Date(activity.date);
+
+      return (
+        <li key={activity.id}>
+          <div>{date.toDateString()}</div>
+          <div>{activity.vegetable}</div>
+          <div>{activity.job}</div>
+          <div>{activity.note}</div>
+        </li>
+      );
+    });
+
+    return (
+      <ul>{activities}</ul>
+    );
+  }
 }
 
 export default App;
