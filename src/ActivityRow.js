1bb43ba3a43b33be3d3a20652a7bdd38b5c52f69
kirikiriyamama <kirikiriyamama@gmail.com>
2020-06-14 23:14:59 +0900

    Update date format

--- src/ActivityRow.js
+++ src/ActivityRow.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
 class ActivityRow extends React.Component {
+  weekday(date) {
+    const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    return days[date.getDay()];
+  }
+
   render() {
     const date = new Date(this.props.date);
 
+    const y = date.getFullYear();
+    const m = (date.getMonth() + 1).toString().padStart(2, '0');
+    const d = date.getDate().toString().padStart(2, '0');
+
     return (
       <tr>
-        <td>{date.toDateString()}</td>
+        <td>{`${y}-${m}-${d} ${this.weekday(date)}`}</td>
         <td>{this.props.vegetable}</td>
         <td>{this.props.job}</td>
         <td>{this.props.note}</td>
