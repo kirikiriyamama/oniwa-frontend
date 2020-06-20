a96f7724dfcf72ef250efb7e92f57a70c7b348f4
kirikiriyamama <kirikiriyamama@gmail.com>
2020-06-21 00:02:27 +0900

    Refactor

--- src/ActivityRow.js
+++ src/ActivityRow.js
@@ -1,21 +1,27 @@
 import React from 'react';
 
 class ActivityRow extends React.Component {
-  weekday(date) {
+  weekday() {
     const days = ['日', '月', '火', '水', '木', '金', '土'];
+
+    const date = new Date(this.props.date);
     return days[date.getDay()];
   }
 
-  render() {
+  formatDate() {
     const date = new Date(this.props.date);
 
     const y = date.getFullYear();
     const m = (date.getMonth() + 1).toString().padStart(2, '0');
     const d = date.getDate().toString().padStart(2, '0');
 
+    return `${y}-${m}-${d} (${this.weekday()})`;
+  }
+
+  render() {
     return (
       <tr>
-        <td>{`${y}-${m}-${d} ${this.weekday(date)}`}</td>
+        <td>{this.formatDate()}</td>
         <td>{this.props.vegetable}</td>
         <td>{this.props.job}</td>
         <td>{this.props.note}</td>
